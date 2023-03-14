87dff88860e7c9f5317209cf6351de649f5e4769
ynoolee <heerarea129@gmail.com>
2023-03-14 13:16:57 +0900

    add: hello-maven 모듈 추가

---
+++ hello-maven/src/main/java/me/whiteship/hello/HelloService.java
@@ -0,0 +1,8 @@
+package me.whiteship.hello;
+
+public class HelloService {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Maven!");
+    }
+}
---
+++ hello-maven/src/main/java/module-info.java
@@ -0,0 +1,2 @@
+module me.whiteship.hello {
+}
